a31e56596b80a7b681ddc12be98f81e0134a3cc2
Ingole987 <pratik.ingole98@gmail.com>
2022-01-28 17:50:10 +0530

    added:Day14Assignment

---
+++ LinkedList.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAndLinkedList
+{
+
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            if (position == 1)
+            {
+                Node newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        var node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            Console.WriteLine("Inserted value is : " + head);
+            return head;
+        }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
+        }
+        internal Node RemoveLastNode()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node NewNode = head;
+            while (NewNode.next.next != null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+            return head;
+        }
+        public bool search(int value)
+        {
+            int index = 0;
+            Node temp = head;
+            while (temp != null)
+            {
+                index++;
+                if (temp.data.Equals(value))
+                {
+                    Console.WriteLine(value + " is Found At Index = " + index);
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
+        }
+        public void Delete(int data)
+        {
+            Node temp = head, previous = null;
+            if (temp != null && temp.data == data)
+            {
+                head = temp.next;
+                return;
+            }
+            while (temp != null && temp.data != data)
+            {
+                previous = temp;
+                temp = temp.next;
+            }
+            if (temp == null)
+                return;
+            previous.next = temp.next;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace DataStructureAndLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedlist = new LinkedList();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(40);
+            linkedlist.Add(70);
+            linkedlist.Display();
+            Console.WriteLine("--------------");
+
+            SortLinkedList Sortlinkedlist = new SortLinkedList();
+            Sortlinkedlist.Add(56);
+            Sortlinkedlist.Add(30);
+            Sortlinkedlist.Add(40);
+            Sortlinkedlist.Add(70);
+            Sortlinkedlist.Display();
+            Console.WriteLine("--------------");
+
+            //linkedlist.RemoveFirstNode();
+            //linkedlist.RemoveLastNode();
+            //linkedlist.search(30);
+            //linkedlist.InsertAtParticularPosition(3, 40);
+            //linkedlist.Delete(40);
+            //linkedlist.Display();
+            //Console.ReadKey();
+        }
+    }
+}
